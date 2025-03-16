4218e9efb395e8f65ab46ff4ac29152ec43830b6
Mwije-thi <phdt01005@polymathcampus.edu.lk>
2025-03-16 12:42:54 +0530

    commit all

---
+++ src/App.js
@@ -0,0 +1,26 @@
+//import logo from './logo.svg';
+import profile from './react logo 3.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={profile} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          MINULI
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
